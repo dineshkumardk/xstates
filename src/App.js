b99f1549d1180b05f686de0b88264da76f8d2893
dineshkumardk <dkdcse@gmail.com>
2024-07-25 22:17:56 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ export default function App() {
       </div>
       {selectedCity && (
         <h2 className="result">
-          You selected <span className="highlight">{selectedCity}</span>
+          You Selected <span className="highlight">{selectedCity}</span>
           <span className="fade">
             {" "}, {selectedState}, {selectedCountry}
           </span>
